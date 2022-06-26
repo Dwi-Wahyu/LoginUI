2064520782b4e4770811ba62cf51303a258ea5d6
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2022-06-26 13:11:54 +0800

    Commit pertama

---
+++ script.js
@@ -0,0 +1,12 @@
+function register() {
+  location.href = "register.html";
+}
+function home() {
+  location.href = "home.html";
+}
+function login() {
+  location.href = "login.html";
+}
+function makeaccount() {
+  alert("Coming Soon");
+}
